bbcfdf2d0d279cf33f7118f7f5cc641e75f31152
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-10-01 16:13:11 -0300

    Add skeleton code

--- src/TableDistances.jl
+++ src/TableDistances.jl
@@ -1,5 +1,40 @@
 module TableDistances
 
-# Write your package code here.
+using Distances
+using StringDistances
+using CoDa: CoDaDistance
+using ScientificTypesBase
+
+import Distances: pairwise
+
+# -------------------------------
+# distances for scientific types
+# -------------------------------
+
+default_distance(::Continuous) = Euclidean()
+# TODO: add other cases
+
+# -------------------------------
+
+"""
+    TableDistance
+
+Distance between rows of Tables.jl tables.
+
+## Example
+
+```julia
+julia> pairwise(TableDistance(), table₁, table₂)
+```
+"""
+struct TableDistance end
+
+function pairwise(::TableDistance, table₁, table₂)
+  # TODO: add implementation
+end
+
+export
+  TableDistance,
+  pairwise
 
 end
