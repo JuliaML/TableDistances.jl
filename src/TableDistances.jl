fb70c7f0ca9c9f522353f4d8db147e7bc83651ee
José A. S. Silva <jass2@ic.ufal.br>
2021-10-01 19:19:36 -0300

    Adds first version of pairwise.

--- src/TableDistances.jl
+++ src/TableDistances.jl
@@ -4,6 +4,8 @@ using Distances
 using StringDistances
 using CoDa
 using ScientificTypesBase
+using ScientificTypes
+using Tables
 
 import Distances: pairwise
 
@@ -21,6 +23,19 @@ default_distance(::Type{<:Compositional}) = CoDa.CoDaDistance()
 # -------------------------------
 
 """
+    cols2dists(table)
+
+Given a table, return a dictionary with column => default_distance. 
+"""
+function cols2dists(table)
+  columns = Tables.columnnames(table)
+  scitypes = schema(table).scitypes
+  distances = default_distance.(scitypes)
+  
+  Dict(columns .=> distances)
+end
+
+"""
     TableDistance
 
 Distance between rows of Tables.jl tables.
@@ -31,10 +46,17 @@ Distance between rows of Tables.jl tables.
 julia> pairwise(TableDistance(), table₁, table₂)
 ```
 """
-struct TableDistance end
+struct TableDistance
+  distance::Dict
+end
 
-function pairwise(::TableDistance, table₁, table₂)
-  # TODO: add implementation
+function pairwise(td::TableDistance, table₁, table₂)
+  distance4column₁ = cols2dists(table₁)
+  distance4column₂ = cols2dists(table₂)
+
+  @assert distance4column₁ == distance4column₂ "incompatible columns types"
+
+  sum([pairwise(distance4column₁[key], table₁[key], table₂[key]) for key in keys(distance4column₁)])
 end
 
 export
