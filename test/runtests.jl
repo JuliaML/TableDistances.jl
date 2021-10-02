5e13edb7d3d8352ca94b43e57bef61ce6736d700
José A. S. Silva <jass2@ic.ufal.br>
2021-10-02 09:27:10 -0300

    Writing P₂ in terms of Distances.pairwise.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,11 @@ using TableDistances
 using Tables
 using Test
 using CoDa
-import Distances: Euclidean
+using Distances
 
 @testset "TableDistances.jl" begin
   table₁ = (a = rand(4), b = rand(Composition{5}, 4))
   table₂ = (a = rand(6), b = rand(Composition{5}, 6))
-
-  distance₁ = Euclidean()
-  distance₂ = CoDaDistance()
 
   euclidcol₁ = Tables.getcolumn(table₁, :a)
   euclidcol₂ = Tables.getcolumn(table₂, :a)
@@ -17,8 +14,8 @@ import Distances: Euclidean
   codacol₁ = Tables.getcolumn(table₁, :b)
   codacol₂ = Tables.getcolumn(table₂, :b)
 
-  P₁ = pairwise(TableDistance(), table₁, table₂)
-  P₂ = [distance₁(euclidcol₁[i], euclidcol₂[j]) + distance₂(codacol₁[i], codacol₂[j]) for i in 1:4, j in 1:6]
+  P₁ = TableDistances.pairwise(TableDistance(), table₁, table₂)
+  P₂ = Distances.pairwise(Euclidean(), euclidcol₁, euclidcol₂) + Distances.pairwise(CoDaDistance(), codacol₁, codacol₂)
 
   @test P₁ ≈ P₂
 end
