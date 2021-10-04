e04387548e42f2606ac2cca3630d79a24f95a3c6
José A. S. Silva <jass2@ic.ufal.br>
2021-10-04 17:56:30 -0300

    Normalize weights in pairwise and create a testset for weights.

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,9 +18,27 @@ using ScientificTypes
   
     D₁ = pairwise(TableDistance(normalize=true), table₁, table₂)
     D₂ = pairwise(TableDistance(normalize=false), table₁, table₂)
-    D₃ = pairwise(Euclidean(), euclidcol₁, euclidcol₂) +
-         pairwise(CoDaDistance(), codacol₁, codacol₂)
+    D₃ = 0.5*pairwise(Euclidean(), euclidcol₁, euclidcol₂) +
+         0.5*pairwise(CoDaDistance(), codacol₁, codacol₂)
     @test sum(D₁ .≤ D₂) > 4*6/2
-    @test D₂ ≈ 0.5*D₃
+    @test D₂ ≈ D₃
+  end
+
+  @testset "Weights" begin
+    table₁ = (a = rand(4), b = rand(Composition{5}, 4))
+    table₂ = (a = rand(6), b = rand(Composition{5}, 6))
+
+    euclidcol₁ = Tables.getcolumn(table₁, :a)
+    euclidcol₂ = Tables.getcolumn(table₂, :a)
+  
+    codacol₁ = Tables.getcolumn(table₁, :b)
+    codacol₂ = Tables.getcolumn(table₂, :b)
+  
+    D₁ = pairwise(TableDistance(normalize=true, weights=Dict(:a => 2., :b => 8.)), table₁, table₂)
+    D₂ = pairwise(TableDistance(normalize=false, weights=Dict(:a => 2., :b => 8.)), table₁, table₂)
+    D₃ = 0.2*pairwise(Euclidean(), euclidcol₁, euclidcol₂) +
+         0.8*pairwise(CoDaDistance(), codacol₁, codacol₂)
+    @test sum(D₁ .≤ D₂) > 4*6/2
+    @test D₂ ≈ D₃
   end
 end
