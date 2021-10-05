2c7a361c60517a25358520a85f4a2ef18c42d8d8
José A. S. Silva <jass2@ic.ufal.br>
2021-10-05 16:42:49 -0300

    Add docstring and move from the top of file the new defined distances.

--- src/distances.jl
+++ src/distances.jl
@@ -2,24 +2,34 @@
 # Licensed under the MIT License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-default_distance(::Type{Continuous})      = Distances.Euclidean()
-default_distance(::Type{Count})           = Distances.Cityblock()
-default_distance(::Type{<:Multiclass})    = MulticlassDistance()
-default_distance(::Type{<:OrderedFactor}) = OrderedFactorDistance()
-default_distance(::Type{Textual})         = StringDistances.Levenshtein()
-default_distance(::Type{<:Compositional}) = CoDa.CoDaDistance()
-
 struct MulticlassDistance <: Metric end
 
+"""
+    MulticlassDistance()(x, y)
+
+Return `true` if x and y are different, else return `false`.
+"""
 (::MulticlassDistance)(x, y) = x != y
 
 result_type(::MulticlassDistance, x, y) = Bool
 
 struct OrderedFactorDistance <: Metric end
 
+"""
+    OrderedFactorDistance()(x, y)
+
+Return the absolute value of the difference between x and y.
+"""
 (::OrderedFactorDistance)(x, y) = abs(levelcode(x) - levelcode(y))
 
 result_type(::OrderedFactorDistance, x, y) = Float64
+
+default_distance(::Type{Continuous})      = Distances.Euclidean()
+default_distance(::Type{Count})           = Distances.Cityblock()
+default_distance(::Type{<:Multiclass})    = MulticlassDistance()
+default_distance(::Type{<:OrderedFactor}) = OrderedFactorDistance()
+default_distance(::Type{Textual})         = StringDistances.Levenshtein()
+default_distance(::Type{<:Compositional}) = CoDa.CoDaDistance()
 
 function default_distances(table)
   columns = Tables.columnnames(table)
