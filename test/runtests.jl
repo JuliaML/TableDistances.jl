aab4b890122c4c8d6f6e28522f906a2cbbf56e36
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-03-28 10:14:33 -0300

    Refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,25 +2,19 @@ using TableDistances
 using CategoricalArrays
 using Distances
 using Tables
+using ColorTypes
 using CoDa
 using Test
 
 @testset "TableDistances.jl" begin
   @testset "Pairwise" begin
-    # test data
+    # compositional data
     table₁ = (a = rand(4), b = rand(Composition{5}, 4))
     table₂ = (a = rand(6), b = rand(Composition{5}, 6))
-    table₃ = (a = ["a", "b", "a", "c"], b = [1, 4, 1, 5])
-    table₄ = (a = categorical(table₃.a), b = categorical(table₃.b))
-    table₅ = (a = categorical(table₃.a, ordered=true), b = categorical(table₃.b, ordered=true))
-
-    # specific columns
     euclidcol₁ = Tables.getcolumn(table₁, :a)
     euclidcol₂ = Tables.getcolumn(table₂, :a)
     codacol₁   = Tables.getcolumn(table₁, :b)
     codacol₂   = Tables.getcolumn(table₂, :b)
-
-    # column normalization
     D₁ = pairwise(TableDistance(normalize=true), table₁, table₂)
     D₂ = pairwise(TableDistance(normalize=false), table₁, table₂)
     D₃ = 0.5*pairwise(Euclidean(), euclidcol₁, euclidcol₂) +
@@ -41,13 +35,16 @@ using Test
     @test D₁ ≈ D₂
 
     # unordered categorical values
+    table₁ = (a = ["a", "b", "a", "c"], b = [1, 4, 1, 5])
+    table₂ = (a = categorical(table₁.a), b = categorical(table₁.b))
+    table₃ = (a = categorical(table₁.a, ordered=true), b = categorical(table₁.b, ordered=true))
     D = [
       0.0  1.0  0.0  1.0
       1.0  0.0  1.0  1.0
       0.0  1.0  0.0  1.0
       1.0  1.0  1.0  0.0
     ]
-    @test pairwise(TableDistance(), table₃) == pairwise(TableDistance(), table₄) == D
+    @test pairwise(TableDistance(), table₁) == pairwise(TableDistance(), table₂) == D
 
     # ordered categorical values
     D = [
@@ -56,6 +53,6 @@ using Test
       0.0  1.0  0.0  2.0
       2.0  1.0  2.0  0.0
     ]
-    @test pairwise(TableDistance(), table₅) == D
+    @test pairwise(TableDistance(), table₃) == D
   end
 end
